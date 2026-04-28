16933d0f17b36a210b1dd0c96a68299bc2796ebb
egidiachricev-web <egidiachricev@gmail.com>
2026-04-28 13:18:36 +0700

    new 28.04.2026

--- MoMA.py
+++ MoMA.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, request
 from models import db, Art, Artist
 import os
-
+from flask import redirect, url_for
 app = Flask(__name__)
 app.secret_key = 'kunci_rahasia_moma'
 
@@ -10,6 +10,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'mo
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+@app.route('/add-art', methods=['GET', 'POST'])
+def add_art():
+    if request.method == 'POST':
+        name = request.form['name']
+        artist_id = request.form['artist_id']
+
+        new_art = Art(name=name, artist_id=artist_id)
+        db.session.add(new_art)
+        db.session.commit()
+
+    return render_template('add_art.html')
 
 @app.route('/')
 def index():
@@ -26,4 +37,4 @@ def art():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
+
